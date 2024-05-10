b957f15dcde9693f122236b7fb1a30bac1836144
yahyojondev <nabijonovdeveloper@gmail.com>
2024-05-10 23:22:28 +0500

    work work

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { Suspense } from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+const App = React.lazy(() => import("./App"));
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { store } from "./context/store";
@@ -8,10 +8,26 @@ import { Provider } from "react-redux";
 import "./sass/main.scss";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>
+    <Suspense
+      fallback={
+        <div className="loading__wrapper">
+          <div>N</div>
+          <div>A</div>
+          <div>B</div>
+          <div>I</div>
+          <div>J</div>
+          <div>O</div>
+          <div>N</div>
+          <div>O</div>
+          <div>V</div>
+        </div>
+      }
+    >
+      <Provider store={store}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </Provider>
+    </Suspense>
   </React.StrictMode>
 );
